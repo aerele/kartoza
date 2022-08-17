ea33a44f3e03b3f9be9f902a06dc7555730930b8
l0gesh29 <logeshperiyasamy24@gmail.com>
2022-08-17 17:11:02 +0530

    retirement_annuity_changes

--- kartoza/custom_py/salary_slip.py
+++ kartoza/custom_py/salary_slip.py
@@ -16,40 +16,33 @@ class CustomSalarySlip(SalarySlip):
 		for i in self.deductions:
 			if frappe.db.get_value("Salary Component", i.salary_component, "is_income_tax_component"):
 				self.deductions[i.idx-1].amount -= (medical_aid + tax_rebate)
-				
+				self.tax_rebate = tax_rebate
+				self.medical_aid = medical_aid
 		super().set_loan_repayment()
 		super().set_precision_for_component_amounts()
 		super().set_net_pay()
 
 	def get_taxable_earnings(self, allow_tax_exemption=False, based_on_payment_days=0):
 		taxable_income = super().get_taxable_earnings(allow_tax_exemption, based_on_payment_days)
-		ra = get_retirement_annuity(self, taxable_income.taxable_earnings * 12)
-		ra_amount = taxable_income.taxable_earnings * ra.ra_percent / 100
-		ra_amount += ra.ra_amount
-		if ra_amount * 12 > ra.limit_value:
-			ra_amount = ra.limit_value / 12
-		ra_percent = ra_amount / taxable_income.taxable_earnings *100
-		if ra_percent > ra.limit_percent:
-			ra_percent = ra.limit_percent
-		ra_amount = ra_percent * taxable_income.taxable_earnings / 100
-		taxable_income.taxable_earnings -= ra_amount
+		ra = get_retirement_annuity(self)
+		if ra:
+			ra_percent = ra.ra_amount / taxable_income.taxable_earnings *100
+			if ra_percent > ra.limit_percent:
+				ra_percent = ra.limit_percent
+			ra_amount = ra_percent * taxable_income.taxable_earnings / 100
+			self.retirement_annuity = ra_amount
+			taxable_income.taxable_earnings -= ra_amount
 		return taxable_income
 
-def get_retirement_annuity(self, amount):
-	ra = frappe.db.get_value("Retirement Annuity", {"effective_from":["<=", self.start_date], "disable":0})
+def get_retirement_annuity(self):
+	ra = frappe.db.get_value("Retirement Annuity", {"effective_from":["<=", self.start_date], "disable":0, "employee":self.employee})
 	res = frappe._dict({})
 	if ra:
 		ra = frappe.get_doc("Retirement Annuity", ra)
-		res['limit_value'] = ra.maximum_amount
 		res['limit_percent'] = ra.maximum_
-		for i in ra.slab:
-			if i.to_amount == 0 and i.from_amount <= amount:
-
-				res["ra_amount"] = i.amount
-				res["ra_percent"] = i.percentage
-			elif i.from_amount <= amount and i.to_amount >= amount:
-				res["ra_amount"] = i.amount
-				res["ra_percent"] = i.percentage
+		res["ra_amount"] = ra.annuity_amount
+		if (ra.maximum_amount // 12) > ra.annuity_amount:
+			res["ra_amount"] = ra.maximum_amount // 12
 	return res
 
 
