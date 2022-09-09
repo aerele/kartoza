15eb8880ad669e1293c218c260bf017c26bed2d4
venkat102 <venkatesharunachalam659@gmail.com>
2022-09-09 15:13:05 +0530

    feat: add seperate jv for Company contribution

--- kartoza/custom_py/payroll_entry.py
+++ kartoza/custom_py/payroll_entry.py
@@ -24,6 +24,7 @@ class CustomPayrollEntry(PayrollEntry):
 
 	@frappe.whitelist()
 	def make_payment_entry(self):
+		super().make_payment_entry()
 		self.check_permission("write")
 
 		salary_slip_name_list = frappe.db.sql(
@@ -38,35 +39,31 @@ class CustomPayrollEntry(PayrollEntry):
 			salary_slip_total = 0
 			for salary_slip_name in salary_slip_name_list:
 				salary_slip = frappe.get_doc("Salary Slip", salary_slip_name[0])
-				for sal_detail in salary_slip.earnings:
-					(
-						is_flexible_benefit,
-						only_tax_impact,
-						creat_separate_je,
-						statistical_component,
-					) = frappe.db.get_value(
-						"Salary Component",
-						sal_detail.salary_component,
-						[
-							"is_flexible_benefit",
-							"only_tax_impact",
-							"create_separate_payment_entry_against_benefit_claim",
-							"statistical_component",
-						],
-					)
-					if only_tax_impact != 1 and statistical_component != 1:
-						if is_flexible_benefit == 1 and creat_separate_je == 1:
-							self.create_journal_entry(sal_detail.amount, sal_detail.salary_component)
-						else:
-							salary_slip_total += sal_detail.amount
-				for sal_detail in salary_slip.deductions:
-					statistical_component = frappe.db.get_value(
-						"Salary Component", sal_detail.salary_component, "statistical_component"
-					)
-					if statistical_component != 1:
-						salary_slip_total -= sal_detail.amount
 				for sal_detail in salary_slip.company_contribution:
 					salary_slip_total += sal_detail.amount
-					print(sal_detail.amount)
 			if salary_slip_total > 0:
 				self.create_journal_entry(salary_slip_total, "salary")
+			
+	def make_accrual_jv_entry(self):
+		jv = super().make_accrual_jv_entry()
+		if jv:
+			doc = frappe.get_doc("Journal Entry", jv)
+			for i in doc.accounts:
+				if i.debit_in_account_currency:
+					frappe.db.set_value(i.doctype, i.name, 'reference_type', self.doctype)
+					frappe.db.set_value(i.doctype, i.name, 'reference_name', self.name)
+		return jv
+
+
+
+def get_payroll_entry_bank_entries(payroll_entry_name):
+	print("In Custom Function")
+	journal_entries = frappe.db.sql(
+		'select jea.name from `tabJournal Entry Account` as jea join `tabJournal Entry` as je on je.name=jea.parent '
+		'where jea.reference_type="Payroll Entry" '
+		'and jea.reference_name=%s and je.docstatus=1 and je.voucher_type="Bank"',
+		payroll_entry_name,
+		as_dict=1
+	)
+
+	return journal_entries
--- kartoza/kartoza/doctype/tax_rebates/tax_rebates.js
+++ kartoza/kartoza/doctype/tax_rebates/tax_rebates.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2022, Aerele and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Tax Rebates', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Tax Rebates", {
+  year: function (frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+    if (row.year == 0) frappe.throw("Year cannot be 0");
+    if (parseString(row.year).length != 4) frappe.throw("Year must be 4 digit");
+  },
 });
