de739ec0caffea740b851704d744220a8612cb06
venkat102 <venkatesharunachalam659@gmail.com>
2023-11-26 22:06:17 +0530

    fix: update patch

--- kartoza/patches/update_payroll_entry.py
+++ kartoza/patches/update_payroll_entry.py
@@ -9,5 +9,5 @@ def execute():
 				account = frappe.db.get_value("Bank Account", employee.custom_payroll_payable_bank_account, "account")
 				if account:
 					account_currency = frappe.db.get_value("Account", account, "account_currency")
-			frappe.db.sql(""" update `tabPayroll Employee Detail` set custom_bank_account_currency='{0}' where parent='{1}' """.format(account_currency, pe.name))
+			frappe.db.sql(""" update `tabPayroll Employee Detail` set custom_bank_account_currency='{0}' where parent='{1}' and employee='{2}' """.format(account_currency, pe.name, employee.employee))
 
