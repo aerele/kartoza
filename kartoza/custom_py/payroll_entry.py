4a1d7920c81a9bb4830999304b6146da75e50b1d
venkat102 <venkatesharunachalam659@gmail.com>
2024-02-06 02:10:06 +0530

    fix: add company contribution in provisional jv

--- kartoza/custom_py/payroll_entry.py
+++ kartoza/custom_py/payroll_entry.py
@@ -135,24 +135,24 @@ class CustomPayrollEntry(PayrollEntry):
 				create_salary_slips_for_employees(employees, args, publish_progress=False)
 				# since this method is called via frm.call this doc needs to be updated manually
 				self.reload()
-	def get_salary_components(self, component_type):
-		salary_components = super().get_salary_components(component_type)
-		salary_slips = self.get_sal_slip_list(ss_status=1, as_dict=True)
+	# def get_salary_components(self, component_type):
+	# 	salary_components = super().get_salary_components(component_type)
+	# 	salary_slips = self.get_sal_slip_list(ss_status=1, as_dict=True)
 
-		if salary_slips and component_type == "earnings":
-			ss = frappe.qb.DocType("Salary Slip")
-			ssd = frappe.qb.DocType("Company Contribution")
-			salary_components += (
-				frappe.qb.from_(ss)
-				.join(ssd)
-				.on(ss.name == ssd.parent)
-				.select(ssd.salary_component, ssd.amount, ssd.parentfield, ss.salary_structure, ss.employee)
-				.where(
-					(ssd.parentfield == "company_contribution") & (ss.name.isin(tuple([d.name for d in salary_slips])))
-				)
-			).run(as_dict=True)
+	# 	if salary_slips and component_type == "earnings":
+	# 		ss = frappe.qb.DocType("Salary Slip")
+	# 		ssd = frappe.qb.DocType("Company Contribution")
+	# 		salary_components += (
+	# 			frappe.qb.from_(ss)
+	# 			.join(ssd)
+	# 			.on(ss.name == ssd.parent)
+	# 			.select(ssd.salary_component, ssd.amount, ssd.parentfield, ss.salary_structure, ss.employee)
+	# 			.where(
+	# 				(ssd.parentfield == "company_contribution") & (ss.name.isin(tuple([d.name for d in salary_slips])))
+	# 			)
+	# 		).run(as_dict=True)
 
-		return salary_components
+	# 	return salary_components
 
 	@frappe.whitelist()
 	def make_payment_entry(self):
@@ -403,6 +403,64 @@ class CustomPayrollEntry(PayrollEntry):
 
 		return total
 
+
+	def get_company_component_total(
+		self,
+		component_type=None,
+		process_payroll_accounting_entry_based_on_employee=False,
+	):
+		salary_components = [] #self.get_salary_components(component_type)
+
+
+		salary_slips = self.get_sal_slip_list(ss_status=1, as_dict=True)
+
+		if salary_slips:
+			ss = frappe.qb.DocType("Salary Slip")
+			ssd = frappe.qb.DocType("Company Contribution")
+			salary_components += (
+				frappe.qb.from_(ss)
+				.join(ssd)
+				.on(ss.name == ssd.parent)
+				.select(ssd.salary_component, ssd.amount, ssd.parentfield, ss.salary_structure, ss.employee)
+				.where(
+					(ssd.parentfield == "company_contribution") & (ss.name.isin(tuple([d.name for d in salary_slips])))
+				)
+			).run(as_dict=True)
+
+
+		if salary_components:
+			component_dict = {}
+
+			for item in salary_components:
+				if not self.should_add_component_to_accrual_jv(component_type, item):
+					continue
+
+				employee_cost_centers = self.get_payroll_cost_centers_for_employee(
+					item.employee, item.salary_structure
+				)
+				employee_advance = self.get_advance_deduction(component_type, item)
+
+				for cost_center, percentage in employee_cost_centers.items():
+					amount_against_cost_center = flt(item.amount) * percentage / 100
+
+					if employee_advance:
+						self.add_advance_deduction_entry(
+							item, amount_against_cost_center, cost_center, employee_advance
+						)
+					else:
+						key = (item.salary_component, cost_center)
+						component_dict[key] = component_dict.get(key, 0) + amount_against_cost_center
+
+					if process_payroll_accounting_entry_based_on_employee:
+						self.set_employee_based_payroll_payable_entries(
+							component_type, item.employee, amount_against_cost_center
+						)
+
+			account_details = self.get_account(component_dict=component_dict)
+
+			return account_details
+
+
 	def create_accrual_jv_entry(self):
 		self.check_permission("write")
 		process_payroll_accounting_entry_based_on_employee = frappe.db.get_single_value(
@@ -422,6 +480,14 @@ class CustomPayrollEntry(PayrollEntry):
 		deductions = (
 			self.get_salary_component_total(
 				component_type="deductions",
+				process_payroll_accounting_entry_based_on_employee=process_payroll_accounting_entry_based_on_employee,
+			)
+			or {}
+		)
+
+		company_contribution = (
+			self.get_company_component_total(
+				component_type="company_contribution",
 				process_payroll_accounting_entry_based_on_employee=process_payroll_accounting_entry_based_on_employee,
 			)
 			or {}
@@ -558,6 +624,20 @@ class CustomPayrollEntry(PayrollEntry):
 
 			earning_entry = jv_accounts[0]
 			provisional_entry = jv_accounts[1:]
+			# earning_entry["debit_in_account_currency"] = self.get_sum_value(provisional_entry, "credit_in_account_currency")
+			for acc_cc, amount in company_contribution.items():
+				payable_amount = self.get_accounting_entries_and_payable_amount(
+					acc_cc[0],
+					acc_cc[1] or self.cost_center,
+					amount,
+					currencies,
+					company_currency,
+					payable_amount,
+					accounting_dimensions,
+					precision,
+					entry_type="credit",
+					accounts=provisional_entry,
+				)
 			earning_entry["debit_in_account_currency"] = self.get_sum_value(provisional_entry, "credit_in_account_currency")
 			accounts = [earning_entry, *provisional_entry]
 
