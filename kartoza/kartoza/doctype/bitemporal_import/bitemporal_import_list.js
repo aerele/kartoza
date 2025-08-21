cda548e6528e612e8ad21692a5516519266e0e52
Kera Chandler <kera.chandler@epiuse.com>
2025-08-21 09:56:38 +0000

    chore: remove bitemporal doctypes (moved to bitemporal app)

--- kartoza/kartoza/doctype/bitemporal_import/bitemporal_import_list.js
+++
@@ -1,5 +0,0 @@
-frappe.listview_settings['Bitemporal Import'] = {
-  onload: function(listview) {
-    // no-op for now
-  }
-};
--- kartoza/kartoza/doctype/bitemporal_import_row/bitemporal_import_row.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Kartoza and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class BitemporalImportRow(Document):
-    pass
